b8b1432818794fb6557a7c14c4930e6717a00514
chandhan-j <64291457+chandhan-j@users.noreply.github.com>
2021-02-24 20:12:42 -0500

    Update uploadFiles.py

--- uploadFiles.py
+++ uploadFiles.py
@@ -12,15 +12,15 @@ class TransferData:
         dbx.files_upload(f.read(), file_to)
 
 def main():
-    access_token = 'd5iyf0hladx34i2'
+    access_token = 'sl.Ar7solFKFMu16Qu9JC2k-_iLKfi86Nqid5aXMYIV6_gCx4Baqj5FSOvkLtWekfEFp7Er6eqYU0Y_2qT1Z1-P861nGvtkyzD0eHJ52vzlAFD3UUuE8zPy3t7ffbkkFZ6c2fL02pE'
     transferData = TransferData(access_token)
 
     file_from = input("Enter the file path to transfer : -")
-    file_to = input("enter the full path to upload to dropbox:- ")  
+    file_to = input("Enter the full path to upload to dropbox:- ")  # This is the full path to upload the file to, including name that you wish the file to be called once uploaded.
 
     # API v2
     transferData.upload_file(file_from, file_to)
     print("file has been moved !!!")
 
 
-main()
+main()  
